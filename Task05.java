93eb261c5085e45c2337dd9160fbbb797a8359b9
Maks <maks988ss@gmail.com>
2021-06-04 04:12:14 +0300

    Add method consider and variable i

--- Task05.java
+++ Task05.java
@@ -4,8 +4,13 @@ public class Task05 {
 
     String name;
     int count;
+    int i;
 
     public Task05(String name) {
         this.name = name;
+    }
+
+    public int consider(int count, int i){
+        return count * i;
     }
 }
